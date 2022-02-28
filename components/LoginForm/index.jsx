1ef1f9abfcdb9c62686a20e8918a06c6b368e5d4
Mufaddal Ibrahimjee <mufaddaljamaly2752@gmail.com>
2022-02-28 11:59:20 +0300

    login logic shifted to pages

--- components/LoginForm/index.jsx
+++ components/LoginForm/index.jsx
@@ -1,90 +1,24 @@
-import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
-import axios from "axios";
+import PropTypes from "prop-types";
 
 import { Stack, Box, Typography } from "@mui/material";
 
 import { LoadingButton } from "@mui/lab";
-
-import Cookies from "js-cookie";
-import useForm from "../../hooks/useForm";
-import { loginFormValidation } from "../../utils/loginFormValidation";
 
 import CustomInput from "../../atoms/CustomInput";
 import MuiAlert from "../../atoms/MuiAlert";
 
 import * as styles from "./styles";
 
-const LoginForm = () => {
-  const router = useRouter();
-  const [formData, handleFormChange] = useForm({
-    email: "",
-    password: "",
-  });
-  const [errors, setErrors] = useState({
-    email: "",
-    password: "",
-    generic: "",
-  });
-  const [loading, setLoading] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const isValid = await loginFormValidation.isValid(formData, {
-      abortEarly: false,
-    });
-    if (isValid) {
-      Cookies.set("name", "value", { path: "/" });
-      const config = {
-        method: "post",
-        url: `https://merchantregistration.ipayprojects.com/auth/login`,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-        data: JSON.stringify(formData),
-      };
-      axios(config)
-        .then((response) => {
-          if (response.success === true) {
-            setLoading(false);
-            const storedObject = {
-              id: response.uuid,
-              ipay1: response.login_otp_token,
-              ipay2: response.response,
-            };
-            localStorage.setItem("ipay", JSON.stringify(storedObject));
-            router.push("/otp");
-          } else {
-            setErrors({ generic: response.response });
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      setErrors({ ...errors, generic: "this is a generic error" });
-    } else {
-      await loginFormValidation
-        .validate(formData, { abortEarly: false })
-        .catch((err) => {
-          const errs = err.inner.reduce(
-            (acc, error) => ({
-              ...acc,
-              [error.path]: error.message,
-            }),
-            {}
-          );
-          setErrors(errs);
-          setLoading(false);
-        });
-    }
-  };
-
+const LoginForm = ({
+  handleSubmit,
+  loading,
+  formData,
+  handleFormChange,
+  errors,
+}) => {
   return (
     <Box sx={styles.formContainer}>
       <Stack component="form" sx={styles.form} onSubmit={handleSubmit}>
@@ -196,4 +130,18 @@ const LoginForm = () => {
   );
 };
 
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  formData: PropTypes.shape({
+    email: PropTypes.string,
+    password: PropTypes.string,
+  }).isRequired,
+  handleFormChange: PropTypes.func.isRequired,
+  errors: PropTypes.shape({
+    email: PropTypes.string,
+    password: PropTypes.string,
+    generic: PropTypes.string,
+  }).isRequired,
+};
 export default LoginForm;
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,97 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+
+import axios from "axios";
 import Carousel from "../components/Carousel";
 import LoginForm from "../components/LoginForm";
 import Auth from "../components/Layouts/Auth";
+import useForm from "../hooks/useForm";
+import { loginFormValidation } from "../utils/loginFormValidation";
 
 const Login = () => {
-  return <Auth left={<Carousel />} right={<LoginForm />} />;
+  const router = useRouter();
+  const [formData, handleFormChange] = useForm({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    generic: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    console.log(process.env.NEXT_PUBLIC_BACKED_BASE_URL);
+    e.preventDefault();
+    setLoading(true);
+    const isValid = await loginFormValidation.isValid(formData, {
+      abortEarly: false,
+    });
+    if (isValid) {
+      Cookies.set("name", "value", { path: "/" });
+      const config = {
+        method: "post",
+        url: `${process.env.NEXT_PUBLIC_BACKED_BASE_URL}auth/login`,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        data: JSON.stringify(formData),
+      };
+      axios(config)
+        .then((response) => {
+          if (response.success === true) {
+            setLoading(false);
+            const storedObject = {
+              id: response.uuid,
+              ipay1: response.login_otp_token,
+              ipay2: response.response,
+            };
+            localStorage.setItem("ipay", JSON.stringify(storedObject));
+            router.push("/otp");
+          } else {
+            setErrors({ generic: response.response });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      setErrors({ ...errors, generic: "this is a generic error" });
+    } else {
+      await loginFormValidation
+        .validate(formData, { abortEarly: false })
+        .catch((err) => {
+          const errs = err.inner.reduce(
+            (acc, error) => ({
+              ...acc,
+              [error.path]: error.message,
+            }),
+            {}
+          );
+          setErrors(errs);
+          setLoading(false);
+        });
+    }
+  };
+
+  return (
+    <Auth
+      left={<Carousel />}
+      right={
+        <LoginForm
+          handleSubmit={handleSubmit}
+          loading={loading}
+          formData={formData}
+          handleFormChange={handleFormChange}
+          errors={errors}
+        />
+      }
+    />
+  );
 };
 
 export default Login;
