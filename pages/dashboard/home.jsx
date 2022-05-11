4d4ee1fcc4ac7fb80b6b4587e1a711beb5c923c3
unknown <mufaddal@ipayafrica.com>
2022-05-11 16:15:53 +0300

    alberts typo fixed

--- pages/dashboard/home.jsx
+++ pages/dashboard/home.jsx
@@ -1,16 +1,7 @@
 import DashboardLayout from "../../components/Layouts/Dashboard";
 
 const Home = () => {
-  return (
-    <div style={{ marginTop: "50px" }}>
-      <p>
-        When true, the source image will be served as-is instead of changing
-        quality, size, or format. Defaults to false. When true, the source image
-        will be served as-is instead of changing quality, size, or format.
-        Defaults to false.
-      </p>
-    </div>
-  );
+  return <></>;
 };
 export default Home;
 
