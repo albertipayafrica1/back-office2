5a6ba2fc048d5145d85e08a4dda738aab1fa360a
unknown <mufaddal@ipayafrica.com>
2022-09-02 16:43:33 +0300

    removed comment on carousel

--- components/Auth/Carousel/index.jsx
+++ components/Auth/Carousel/index.jsx
@@ -6,10 +6,7 @@ import * as styles from "./styles";
 
 const Carousel = () => {
   // get this image array from server
-  const imageArray = [
-    { id: 1, src: "/login-banner.png", alt: "Ipay" },
-    // { id: 2, src: "/login-banner2.png", alt: "Ipay" },
-  ];
+  const imageArray = [{ id: 1, src: "/login-banner.png", alt: "Ipay" }];
   return (
     <Box sx={styles.imageContainer}>
       <CarouselAtom imageArray={imageArray} onChange={() => {}} />
